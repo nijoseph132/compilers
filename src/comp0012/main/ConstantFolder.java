e7fd7404c24a139e34d24f76c167ea37108502de
Dash0219 <114499951+Dash0219@users.noreply.github.com>
2025-04-01 14:31:06 +0100

    dynamic folding not done

--- src/comp0012/main/ConstantFolder.java
+++ src/comp0012/main/ConstantFolder.java
@@ -3,6 +3,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Iterator;
 
 import org.apache.bcel.classfile.ClassParser;
@@ -15,13 +16,16 @@ import org.apache.bcel.generic.InstructionHandle;
 import org.apache.bcel.generic.InstructionList;
 import org.apache.bcel.util.InstructionFinder;
 import org.apache.bcel.generic.MethodGen;
+import org.apache.bcel.generic.StoreInstruction;
 import org.apache.bcel.generic.TargetLostException;
 
 
 import org.apache.bcel.generic.ConstantPushInstruction;
+import org.apache.bcel.generic.ICONST;
 import org.apache.bcel.generic.LDC;
 import org.apache.bcel.generic.InstructionTargeter;
 import org.apache.bcel.generic.LDC2_W;
+import org.apache.bcel.generic.LoadInstruction;
 import org.apache.bcel.generic.Instruction;
 
 
@@ -53,21 +57,117 @@ public class ConstantFolder
 		for (Method method : cgen.getMethods()) {
 			MethodGen mg = new MethodGen(method, cgen.getClassName(), cpgen);
 			InstructionList il = mg.getInstructionList();
+            // System.out.println(il);
 			if (il == null) continue;
 
-			//Task 1 Call
-			simpleFolding(il, cpgen);
+            // Code methodCode = method.getCode();
+            // InstructionList il = new InstructionList(methodCode.getCode());
+            // MethodGen mg = new MethodGen(method.getAccessFlags(), method.getReturnType(), method.getArgumentTypes(), null, method.getName(), cgen.getClassName(), il , cpgen);
+
+            dynamicFolding(il, mg);
+            simpleFolding(il, cpgen);
+            // Just a skeleton
+
+            // boolean hasChanges = false;
+            // do {
+            //     hasChanges = false;
+            //     hasChnages |= constantFolding(il, cpgen);
+            //     hasChanges |= simpleFolding(il, cpgen);
+            // } while (hasChanges);
 
 			//Call your methods here
 
 			mg.setInstructionList(il);
 			mg.setMaxStack();
 			mg.setMaxLocals();
-			cgen.replaceMethod(method, mg.getMethod());
-    }
+            cgen.replaceMethod(method, mg.getMethod());
+        }
         
 		this.optimized = gen.getJavaClass();
 	}
+
+
+    private void dynamicFolding(InstructionList il, MethodGen mg) {
+        HashMap<Integer, Integer> varMap = new HashMap<>();
+        int maxLocals = mg.getMaxLocals(); // Track the highest used variable index
+    
+        for (InstructionHandle handle = il.getStart(); handle != null;) {
+            InstructionHandle next = handle.getNext();
+            Instruction inst = handle.getInstruction();
+            // System.out.println("instruction list: " + il);
+    
+            if (inst instanceof StoreInstruction store) {
+                int varIndex = store.getIndex();
+                int newVarIndex;
+    
+                if (varMap.containsKey(varIndex)) {
+                    newVarIndex = ++maxLocals;
+                } else {
+                    newVarIndex = varIndex;
+                    // varMap.put(varIndex, varIndex);
+                }
+                
+                varMap.put(varIndex, newVarIndex);
+                // Replace the instruction with a new instance of the same type
+                Instruction newStore = createNewStoreInstruction(store, newVarIndex);
+
+                // System.out.println("store, new var index: " + varMap.get(varIndex));
+                // System.out.println("store instruction" + store);
+                // System.out.println("instruction list: " + il);
+
+                // handle.setInstruction(newStore);
+                il.insert(handle, newStore);
+				try {
+					il.delete(handle);
+				} catch (TargetLostException e) {
+					e.printStackTrace();
+				}
+            } else if (inst instanceof LoadInstruction load) {
+                int varIndex = load.getIndex();
+                if (varMap.containsKey(varIndex)) {
+                    int newVarIndex = varMap.get(varIndex);
+    
+                    // Replace the instruction with a new instance of the same type
+                    Instruction newLoad = createNewLoadInstruction(load, newVarIndex);
+                    
+                    // System.out.println("load: new var index: " + varMap.get(varIndex));
+                    // System.out.println("load instruction" + load);
+
+                    // handle.setInstruction(newLoad); 
+                    il.insert(handle, newLoad);
+                    try {
+                        il.delete(handle);
+                    } catch (TargetLostException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            handle = next;
+        }
+    
+        mg.setMaxLocals(maxLocals); // Ensure the method has enough space for new variables
+    }
+    
+    // Helper method to create a new instance of a StoreInstruction
+    private StoreInstruction createNewStoreInstruction(StoreInstruction store, int index) {
+        if (store instanceof org.apache.bcel.generic.ISTORE) return new org.apache.bcel.generic.ISTORE(index);
+        if (store instanceof org.apache.bcel.generic.LSTORE) return new org.apache.bcel.generic.LSTORE(index);
+        if (store instanceof org.apache.bcel.generic.FSTORE) return new org.apache.bcel.generic.FSTORE(index);
+        if (store instanceof org.apache.bcel.generic.DSTORE) return new org.apache.bcel.generic.DSTORE(index);
+        return new org.apache.bcel.generic.ASTORE(index); // Default case (ASTORE for object types)
+    }
+    
+    // Helper method to create a new instance of a LoadInstruction
+    private LoadInstruction createNewLoadInstruction(LoadInstruction load, int index) {
+        if (load instanceof org.apache.bcel.generic.ILOAD) return new org.apache.bcel.generic.ILOAD(index);
+        if (load instanceof org.apache.bcel.generic.LLOAD) return new org.apache.bcel.generic.LLOAD(index);
+        if (load instanceof org.apache.bcel.generic.FLOAD) return new org.apache.bcel.generic.FLOAD(index);
+        if (load instanceof org.apache.bcel.generic.DLOAD) return new org.apache.bcel.generic.DLOAD(index);
+        return new org.apache.bcel.generic.ALOAD(index); // Default case (ALOAD for object types)
+    }
+    
+    
+
 
 /**
 	 * Performs simple constant folding over the instruction list.
@@ -77,7 +177,7 @@ public class ConstantFolder
 		InstructionFinder finder = new InstructionFinder(il);
 
 		for (FoldOp op : FoldOp.values()) {
-			for (Iterator<?> iter = finder.search(op.pattern); iter.hasNext(); ) {
+            for (Iterator<?> iter = finder.search(op.pattern); iter.hasNext(); ) {
 				InstructionHandle[] match = (InstructionHandle[]) iter.next();
 
 				try {
